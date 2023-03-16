335e66db51fd64dbc940dc32b00f02924d05e7e6
Jean Malm <jean.malm@mdh.se>
2023-03-16 16:44:21 +0100

    [refactoring] Separated check into different functions

--- checks/reqcheck.py
+++ checks/reqcheck.py
@@ -4,40 +4,62 @@ import nltk
 nltk.download('punkt')
 nltk.download("stopwords")
 nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
+from nltk.stem import WordNetLemmatizer
 
 def remove_nonessential_words(words, words_to_remove):
     return [w for w in words
             if w.casefold() not in words_to_remove]
 
+def any_of_in(words, source):
+    for word in words:
+        if word in source:
+            return True
+    return False
 
-def classify(req_text: str):
+def classify_as_metric_check(tagged_words, numbers):
     res = dict()
-    token_words = word_tokenize(req_text)
-    stopwords_to_include = {"not", "between"}
-    words = remove_nonessential_words(token_words, set(stopwords.words("english")).difference(stopwords_to_include))
-    tagged_words = nltk.pos_tag(words)
-    numbers = [n for n, t in tagged_words if t == "CD"]  # CD = numeral/cardinal
-    print(tagged_words)
-    if len(numbers):
-        res["check_type"] = "metric"
-
+    res["check_type"] = "metric"
     if len(numbers) > 1:
         res["val"] = re.sub("[ ']", "", str(sorted(numbers, reverse=True)))
     elif len(numbers) > 0:
         res["val"] = str(numbers[0])
-    if "between" in req_text:
+
+    lemmatized_words = [n for n, _ in tagged_words]
+
+    if "between" in lemmatized_words:
         res["target_func"] = "interval"
-    elif "exceed" in req_text:
+    elif "exceed" in lemmatized_words:
         res["target_func"] = ">"
 
-    if "not" in req_text:
+    if "not" in lemmatized_words:
         res["target_func"] = f"not({res['target_func']})"
+    properties = [n for n, t in tagged_words if str(t).startswith("NN") or t.startswith("JJ")]
     res["property"] = "variable"
-    if "shall" in req_text:
-        res["issue_level"] = "error"
+    return res
+
+def get_issue_level(tagged_words):
+    issue_level_qualifier = [n for n, t in tagged_words if t == "MD"]
+    if any_of_in(["shall", "must", "cannot", "need", "will", "couldn't"], issue_level_qualifier):
+        return "error"
     else:
-        res["issue_level"] = "warning"
+        return "warning"
+
+
+def classify(req_text: str):
+    res = dict()
+    lemmatizer = WordNetLemmatizer()
+    token_words = word_tokenize(req_text)
+    stopwords_to_include = {"not", "between"}
+    words = remove_nonessential_words(token_words, set(stopwords.words("english")).difference(stopwords_to_include))
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
+    tagged_words = nltk.pos_tag(lemmatized_words)
+    numbers = [n for n, t in tagged_words if t == "CD"]  # CD = numeral/cardinal
+    issue_level_qualifier = [n for n, t in tagged_words if t == "MD"]
+    print(tagged_words)
+    if len(numbers):
+        res = classify_as_metric_check(tagged_words, numbers)
+    res["issue_level"] = get_issue_level(tagged_words)
     return res
