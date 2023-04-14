cbefad36c44beba7350ef2fded66ddb45fb03a9c
Jean Malm <jean.malm@mdh.se>
2023-04-14 13:38:44 +0200

    added utility to go from file path to parsed program

--- parser/SafeProgTest.py
+++ parser/SafeProgTest.py
@@ -9,6 +9,7 @@ from pathlib import Path
 
 
 # Helpers
+
 def get_worksheets_from_input(input_program: str):
     index_start_of_XML = input_program.index(
         '<?xml version="1.0" encoding="utf-16" standalone="yes"?>'
@@ -18,6 +19,7 @@ def get_worksheets_from_input(input_program: str):
     input_codeSheet = input_program[index_start_of_XML:index_end_of_XML]
 
     return input_varWorkSheet, input_codeSheet
+
 
 
 def parse_variable_worksheet(input_varWorkSheet):
@@ -46,6 +48,17 @@ def parse_code_worksheet(input_codeWorkSheet:str):
 
 
 
+def parse_pou_file(pou_file_path:str):
+    def clean_pou_string(input_pou_prog:str):
+        """Removes useless parts of the program before parsing"""
+        return input_pou_prog.replace("﻿", "")
+
+    program_string = clean_pou_string(Path(pou_file_path).read_text())
+    varSheet, codeSheet = get_worksheets_from_input(program_string)
+
+    resultProgram = parse_variable_worksheet(varSheet)
+    resultProgram.behaviourElements, resultProgram.behaviour_id_map = parse_code_worksheet(codeSheet)
+    return resultProgram
 def print_xml_parsing():
     inputText = """<?xml version="1.0" encoding="utf-16" standalone="yes"?>
         <FBD>
