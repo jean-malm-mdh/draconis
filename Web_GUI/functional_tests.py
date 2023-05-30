25dd1099f3691930f44cd2878d4b60c2eb6d17bb
Jean Malm <jean.malm@mdh.se>
2023-05-30 16:25:04 +0200

    Added functional test for editing review comment

--- Web_GUI/functional_tests.py
+++ Web_GUI/functional_tests.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -9,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def test_can_view_basic_report(self):
+    def test_can_view_and_edit_basic_report(self):
         self.browser.get("http://localhost:8000")
 
         self.assertIn("Report Findings", self.browser.title)
@@ -18,6 +20,24 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID, value="id_reports_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
         self.assertIsNot(list(rows), [])
+        ROW_TO_SELECT = -1
+        time.sleep(2)
+        # Select the last report to add review comment
+        table.find_elements(By.NAME, "selected")[ROW_TO_SELECT].click()
+        time.sleep(1)
+
+        # Write the review
+        review_textbox = self.browser.find_element(By.ID, "review_comment")
+        review_textbox.send_keys("This is my amended review comment for the issue")
+        time.sleep(2)
+        #Send the review
+        review_textbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+        affected_review = self.browser.find_element(By.ID, "rev2")
+
+        self.assertIn("This is my amended review comment for the issue",
+                      affected_review.text)
 
 if __name__ == "__main__":
     unittest.main()
