1ce2b4c43b82edef1d4ba6a77a0e04fa3a866b79
Jean Malm <jean.malm@mdh.se>
2023-05-30 14:45:59 +0200

    Basic report view returns minimal HTML

--- Web_GUI/reports/tests.py
+++ Web_GUI/reports/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 import sys
@@ -10,3 +11,11 @@ class HomepageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>Report Findings</title>", html)
+        self.assertTrue(html.endswith("</html>"))
