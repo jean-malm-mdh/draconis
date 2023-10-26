35ace5270855cb64d93ed608cefff44ba3797dfe
Jean Malm <jean.malm@mdh.se>
2023-10-26 10:54:20 +0200

    Refactored evaluate combinator selection

--- semantics/evaluator.py
+++ semantics/evaluator.py
@@ -2,14 +2,19 @@ from functools import reduce
 
 
 def evaluate(Block, *args):
+    def get_combinator(block_family_type: str):
+        combinators = {"ADD": lambda acc, _v: acc + _v,
+                       "SUB": lambda acc, _v: acc - _v,
+                       "AND": lambda acc, _v: acc and _v}
+        return combinators.get(block_family_type, lambda x, y: None)
+
     if len(args) == 0:
         return None
     for v in args:
         v_type = type(v)
         if v_type is not int and v_type is not bool:
             return None
-    combinator = (lambda acc, _v: acc + _v) if Block.getBlockFamily() == "ADD" else (
-        lambda acc, _v: acc - _v) if Block.getBlockFamily() == "SUB" else (lambda acc, _v: acc and _v)
+    combinator = get_combinator(block_family_type=Block.getBlockFamily())
     res = args[0]
     for v in args[1:]:
         res = combinator(res, v)
