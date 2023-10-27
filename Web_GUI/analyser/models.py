5eed22cb8d3fe22077f759aed26cb073b61fc8a3
Jean Malm <jean.malm@mdh.se>
2023-10-27 14:04:40 +0200

    Updated web app to working version, but using model database

--- Web_GUI/analyser/models.py
+++ Web_GUI/analyser/models.py
@@ -5,10 +5,18 @@ class BlockModel(models.Model):
     program_name = models.TextField(blank=False)
     program_content = models.FileField(upload_to="DATASTORE/")
     uploaded_at = models.DateTimeField("Date uploaded", auto_now_add=True)
-    program_picture = models.ImageField()
+    program_variables = models.JSONField(verbose_name="Program Variables", blank=False)
+    program_metrics = models.JSONField(verbose_name="Program Metrics", blank=False)
+    program_vardependencies = models.JSONField("Dependency analysis", blank=False)
+    # program_picture = models.ImageField()
 
 class ReportModel(models.Model):
+    class ReportStatus(models.IntegerChoices):
+        UNVIEWED = 0
+        UNDER_REVIEW = 1
+        REVIEWED = 2
     block_program = models.ForeignKey(BlockModel, on_delete=models.CASCADE)
     check_name = models.TextField()
     check_verbose_name = models.TextField()
     report_content = models.TextField()
+    report_status = models.IntegerField(choices=ReportStatus.choices)
--- Web_GUI/analyser/views.py
+++ Web_GUI/analyser/views.py
@@ -1,72 +1,79 @@
+import json
 import os.path
 
 from django.core.files.uploadedfile import UploadedFile
+from django.db.models.fields.files import FieldFile
 from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 import os
 import sys
 
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../.."))
-from parser.helper_functions import parse_pou_file
+from parser.helper_functions import parse_pou_file, parse_pou_content
 
 # Create your views here.
 from django import forms
-
+from .forms import BlockModelForm
 from AST import PathDivide
 from parser.renderer import generate_image_of_program
 
 
-class UploadFileForm(forms.Form):
-    file = forms.FileField()
-
-
 ANALYSER_DATA_STORE_PATH = (
-    "/Users/jmm01/Documents/SmartDelta/safeprogparser/Web_GUI/analyser/static"
+    os.path.abspath(os.path.dirname(__file__)) + "/static"
 )
 
+def get_file_content_as_single_string(file_field: FieldFile):
+    return "\n".join([str(s, 'UTF-8') for s in file_field.readlines()])
 
-def save_file_to_django_space(upload_file: UploadedFile):
-    file_path_on_target = os.path.join(ANALYSER_DATA_STORE_PATH, upload_file.name)
-    with open(file_path_on_target, "wb") as destination:
-        for chunk in upload_file.chunks():
-            destination.write(chunk)
-    return file_path_on_target
-
+def make_and_save_program_model_instance(form):
+    model_instance = form.save(commit=False)
+    program_content = get_file_content_as_single_string(model_instance.program_content)
+    program = parse_pou_content(program_content)
+    reports = program.check_rules()
+    metrics = program.getMetrics()
+    variable_info = program.getVarDataColumns(
+        "name", "paramType", "valueType", "initVal", "description"
+    )
+    backward_trace = {}
+    for name, paths in program.getBackwardTrace().items():
+        backward_trace[name] = [
+            program.behaviour_id_map[e[-1]].expr.expr
+            for e in PathDivide.unpack_pathlist([paths])
+        ]
+    # Populate model instance object based on analysis
+    model_instance.program_name = program.progName
+    model_instance.program_metrics = json.dumps(metrics)
+    model_instance.program_variables = json.dumps(variable_info)
+    model_instance.program_vardependencies = json.dumps(backward_trace)
+    # Finally, save the analysed model instance to DB
+    model_instance.save()
+    reportData = {
+        "pou_progName": program.progName,
+        "rule_reports": reports,
+        "metrics": metrics,
+        "backward_trace": backward_trace,
+        "variable_info": variable_info,
+    }
+    return program, reportData
 
 def home_page(request):
     if request.method == "POST":
-        form = UploadFileForm(request.POST, request.FILES)
+        form = BlockModelForm(request.POST, request.FILES)
         if form.is_valid():
-            file_path = save_file_to_django_space(request.FILES["file"])
-            program = parse_pou_file(file_path)
+            program, reportData = make_and_save_program_model_instance(form)
             generated_image_django_path = "images/.generated/tmp.png"
             image_file_path = os.path.join(ANALYSER_DATA_STORE_PATH, generated_image_django_path)
             generate_image_of_program(program, image_file_path, scale=7.0)
-            reports = program.check_rules()
-            metrics = program.getMetrics()
-            variable_info = program.getVarDataColumns(
-                "name", "paramType", "valueType", "initVal", "description"
-            )
-            backward_trace = {}
-            for name, paths in program.getBackwardTrace().items():
-                backward_trace[name] = [
-                    program.behaviour_id_map[e[-1]].expr.expr
-                    for e in PathDivide.unpack_pathlist([paths])
-                ]
 
-            reportData = {
-                "file_name": request.FILES["file"].name,
-                "pou_progName": program.progName,
-                "rule_reports": reports,
-                "metrics": metrics,
-                "backward_trace": backward_trace,
-                "variable_info": variable_info,
-            }
             hasImageFile = os.path.exists(image_file_path)
             if hasImageFile:
                 reportData["Image"] = generated_image_django_path
-            return render(request, "pou_report.html", reportData)
+            return render(request, "analyser/pou_report.html", reportData)
         else:
-            return render(request, "home.html", {"form": form})
+            return render(request, "analyser/home.html", {"form": form})
     else:
-        form = UploadFileForm()
-        return render(request, "home.html", {"form": form})
+        form = BlockModelForm()
+        return render(request, "analyser/home.html", {"form": form})
+
+
